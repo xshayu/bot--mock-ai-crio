a5ecccd15e7bc4dc9b76d7ef673e5f2fa3260a02
Ayush Wardhan (Second Account) <45749740+xshayu@users.noreply.github.com>
2025-04-10 02:59:54 +0000

    fix: changed image url

--- bot-mock-ai-crio/app/chat/messageBubble.tsx
+++ bot-mock-ai-crio/app/chat/messageBubble.tsx
@@ -14,7 +14,7 @@ const MessageBubble = memo(({
     const isUser = message.by === 'user';
     const avatar = isUser ?
         'https://avatars.githubusercontent.com/u/45749740?s=400&v=4'
-        : 'https://s3-alpha-sig.figma.com/img/4b1c/47d6/93bc8af758e2de31a7de8493b52750af?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ENA~YSaf9THN5paaRkbIJjELW2iNRsBCFvM-id9EjLB-Fs1pUHNkN8RIq8zGwKNvhzTv4e5rR1zPKlfdODJwV~bRvYA1Aze26PIlbPAHL9MV9e~sc0igwzro-nv1NkUQBw0ST778qQmxh9i3iVvnpWjJX69wj3nRsHY~03aWo6K37hOU~ecwtfU7MUgHOxVGkdXl9W~im7LPOZzkU4nGS0hsmLwB-FFPStf887iGCx6cfWZU2R5FknqYqZZkkVhr12pdzT5-6lvPGKmY4U0XRZGtGKxaQGxzw2Bhrhmua7QalHaTv70G9yMCFG--23DrJmJ0Vqi8hUrDzxK88FGMnw__';
+        : 'https://avatars.githubusercontent.com/u/95334503';
     
 
     return (
